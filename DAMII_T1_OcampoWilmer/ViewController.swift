029b1bbe67dc6c405246ec64014fa5e2c81e0979
Wilmer Ocampo Quispe <Wilmer_Ocampo@hotmail.com>
2024-11-25 21:25:56 -0500

    Uploading project

---
+++ DAMII_T1_OcampoWilmer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DAMII_T1_OcampoWilmer
+//
+//  Created by Wilmer Ocampo on 25/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
